3103ca7d0f36ffcff9a51280bd06be6fd1a1dc86
Hugo Tigre <hugo.tigre@gmail.com>
2019-04-05 17:52:12 +0100

    1st commit

---
+++ src/main/kotlin/com/pakybytes/springbootessentials/SpringBootEssentialsApplication.kt
@@ -0,0 +1,11 @@
+package com.pakybytes.springbootessentials
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootEssentialsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootEssentialsApplication>(*args)
+}
